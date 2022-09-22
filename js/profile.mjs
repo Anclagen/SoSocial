9520ed8403c9523ce28e4553090cbaa541318aed
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-09-22 02:07:08 +0200

    added basic page rendering and query string
    1. Querystring to display others users profile, default is own.
    2. get profile data to render banner/avatar and name, default images provided.
    3. Added posts to feed, need to filter for user.

--- js/profile.mjs
+++ js/profile.mjs
@@ -1,3 +1,54 @@
-import {initialiseAPIHandler} from "./components/main.mjs"
+import {initialiseAPIHandler, createBasicPost} from "./components/main.mjs"
 const API = initialiseAPIHandler();
 //it begins!
+
+const profileBanner = document.querySelector("#profileBanner");
+const profileImage = document.querySelector("#profileImage");
+const heading = document.querySelector("h1");
+const editContainer = document.querySelector("#edit");
+const aboutContainer = document.querySelector("#about");
+const postsContainer = document.querySelector("#post-feed");
+
+/**
+ * Checks if a query string is present to define a user.
+ * @param {Class} API insert defined handleAPI class into this.
+ * @returns {String} Username returned for fetch request.
+ */
+function defineUser(API){
+  let user = API.name;
+  const qstring = new URLSearchParams(window.location.search);
+  if(qstring.has("profile")){
+    user = qstring.get("profile");
+  } else {
+
+  }
+  return user
+}
+
+
+(async () => {
+  try{
+
+    let user = defineUser(API);
+    const data = await API.getProfile(user); 
+
+    profileBanner.src = `${data.banner}`;
+    profileImage.src = `${data.avatar}`;
+    heading.innerText = `${data.name}`; 
+    // editContainer.innerText = `${data.meta.body}` // awaiting extra content.
+
+    if(API.name === data.name){
+      console.log("build a button!");
+    }
+
+    const postData = await API.getPosts(); 
+    postData.forEach(post => {
+      postsContainer.appendChild(createBasicPost(post));
+    });
+
+
+  } catch(error) {
+    console.log(error)
+  }
+
+})();
