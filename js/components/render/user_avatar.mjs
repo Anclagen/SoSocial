c4d8e869896caf6bdd4fd42fa4abb0d460006ca2
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-09-25 01:34:43 +0200

    editing follower html setup

--- js/components/render/user_avatar.mjs
+++ js/components/render/user_avatar.mjs
@@ -6,10 +6,10 @@ export function createAvatar({avatar, name}){
     userAvatar = avatar;
   }
   const user = `<a href="profile.html?profile=${name}" class="d-flex text-white text-decoration-none mb-1">
-                  <div class="post-header rounded-3 px-1 d-flex">
+                  <div class="post-header rounded-3 pe-1 d-flex">
                     <img class="img-fluid rounded-circle me-2" src="${userAvatar}" alt="user profile picture" onerror='this.src="images/default-avatar.png"' />
                   <div>
-                    <p class="mb-0 fs-5 pt-1">${name}</p>
+                    <p class="mb-0 fs-5 pt-1 follower-names">${name}</p>
                   </div>
                 </div>
               </a>`
