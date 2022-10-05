318b15ff729d3f96aeac943ab8364978db25c4df
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-10-05 20:53:10 +0200

    Can reply to replies now

--- js/modules/api/posts/createReply.mjs
+++ js/modules/api/posts/createReply.mjs
@@ -1,28 +1,38 @@
 import { API } from "../../main.mjs";
 import { createAReply } from "../../render/post_replies.mjs";
+import { showContainerNoHeight } from "../../main.mjs";
 
 /**
- * Creates a reply to a post, taking post id from button data attribute for request
+ * Creates a reply to a post, taking post id from form data attribute for request
  * Then prepends response to parents reply container and closes form.
  * @param {Event} submit 
  */
 export async function createNewReply(submit) {
   try{
-    const id = submit.target.getAttribute('data-postID');
+    let parentId = "";
+    if(submit.target.getAttribute('data-parentID')){
+      parentId = Number(submit.target.getAttribute('data-parentID'));
+      console.log(parentId)
+    }
+    let id = Number(submit.target.getAttribute('data-postID'));
     submit.preventDefault();
     const formData = new FormData(submit.target);
     const bodyData = Object.fromEntries(formData.entries());
+    if(parentId){
+      bodyData.replyToId = id;
+      id = parentId;
+    }
     const response = await API.replyToPost(id, JSON.stringify(bodyData));
-    console.log(response);
-    submit.target.reset()
+    submit.target.reset();
     //close form
-    submit.target.parentNode.classList.remove("open")
-    submit.target.parentNode.style.height = 0;
-    setTimeout(function () {
-      submit.target.parentNode.classList.toggle('hidden');
-      }, 500);
-    //reply container
+    showContainerNoHeight(submit.target.parentNode);
+    // add reply
     const reply = submit.target.parentNode.parentNode.querySelector(".replies-container");
+    //check for reply to reply, parentId not provided   
+    response.postId= id;
+    if(parentId){
+      response.postId= parentId;
+    }
     reply.prepend(createAReply(response));
   } catch (error){
     console.log(error);
